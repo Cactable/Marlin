fab26da48c9e47a8c893e2642f8c71c32c109efa
Cactable <40891371+Cactable@users.noreply.github.com>
2021-02-19 14:51:18 +0100

    FIX FOR CENTER

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -982,8 +982,8 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -14, -46, 0 }
-
+//#define NOZZLE_TO_PROBE_OFFSET { -32, -37, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -12, 0 }
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 10
